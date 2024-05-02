d3c1371f0b138fb17cfd3bc21048d721af9126fb
cfplhub <163717909+cfplhub@users.noreply.github.com>
2024-05-02 22:31:48 +0800

    Add files via upload

---
+++ test_1102/test.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+   
+
+
+     
+        printf("  ***     ***  \n");
+        printf(" *****   ***** \n");
+        printf("******* *******\n");
+        printf(" ************* \n");
+        printf("  ***********  \n");
+        printf("   *********   \n");
+        printf("    *******    \n");
+        printf("     *****     \n");
+        printf("      ***      \n");
+        printf("       *       \n");
+
+        return 0;
+   
+   
+
+
+}
