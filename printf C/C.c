ca42a22f413500caebe2cde7e570e283ac0198de
cfplhub <163717909+cfplhub@users.noreply.github.com>
2024-06-03 20:45:17 +0800

    Add files via upload

---
+++ printf C/C.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS	
+#include <stdio.h>
+
+int main()
+{
+	extern int age;
+	printf("age=%d", age);
+
+	//int sum1 = 0;
+	//int sum2 = 0;
+	//int sum3 = 10;
+	//scanf("%d%d", &sum1, &sum2);
+
+	//sum3 = sum1 + sum2;
+	//
+	//printf("sum3=%d", sum3);
+
+	return 0;
+}
