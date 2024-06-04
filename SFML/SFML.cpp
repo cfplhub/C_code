fa4a844f5e1cf00100582aa82330a77ae74f367b
cfplhub <163717909+cfplhub@users.noreply.github.com>
2024-06-04 19:42:43 +0800

    Add files via upload

---
+++ SFML/SFML.cpp
@@ -0,0 +1,37 @@
+﻿#include <SFML/Graphics.hpp>
+
+
+int main()
+{
+    // 创建一个窗口
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Console Application");
+
+    // 创建一个圆形
+    sf::CircleShape shape(50);
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition(400, 300);
+
+    // 游戏循环，持续更新窗口和渲染图形
+    while (window.isOpen())
+    {
+        // 处理窗口事件
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // 关闭窗口事件
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // 清空窗口并用白色填充
+        window.clear(sf::Color::White);
+
+        // 绘制圆形
+        window.draw(shape);
+
+        // 显示窗口中的内容
+        window.display();
+    }
+
+    return 0;
+}
